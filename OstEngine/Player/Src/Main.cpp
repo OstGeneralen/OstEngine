5f78ecbfe4be203833eb0fed0c2d0c46881c51e3
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-08 01:54:44 +0100

    Basic 'Player' project that acts to load the game dll and engine

---
+++ OstEngine/Player/Src/Main.cpp
@@ -0,0 +1,37 @@
+// OstEngine - Copyright(c) 2025 Kasper Esbjörnsson (MIT License)
+
+#include <OstEngine/Game/GameModuleInstance.h>
+#include <OstEngine/Game/GameModuleInternal.h>
+
+#if APP_WIN32
+#include <Windows.h>
+
+int CALLBACK WinMain(_In_ HINSTANCE aInst, _In_opt_ HINSTANCE pInst, _In_ LPSTR cmd, _In_ int cmdShw)
+{
+    return 0;
+}
+#else
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+    std::cout << "Attempting to load " <<  GAME_MODULE_DLL_NAME << std::endl;
+
+    ostengine_internal::CGameModuleLoader moduleLoader;
+
+    if (moduleLoader.LoadModule(GAME_MODULE_DLL_NAME) == ostengine_internal::ModuleLoaderStatus_Success)
+    {
+        ost::IGameModuleInstance* moduleInstance = moduleLoader.CreateGameModuleInstance();
+        moduleInstance->GetGameInstance().Run();
+        moduleLoader.ReleaseGameModuleInstance(moduleInstance);
+        moduleLoader.ReleaseModule();
+    }
+
+
+    return 0;
+}
+#endif
+
+
+
