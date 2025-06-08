19a1bcca2747fdff2ffb9ad91f782387979fca66
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-08 18:35:28 +0100

    Player Main now use command line parser

--- OstEngine/Player/Src/Main.cpp
+++ OstEngine/Player/Src/Main.cpp
@@ -2,6 +2,8 @@
 
 #include <OstEngine/Game/GameInstance.h>
 #include <OstEngine/Game/GameModuleInternal.h>
+#include <OstEngine/Application/CommandLine/CommandLineParser.h>
+#include <OstEngine/Application/CommandLine/CommandLineArgs.h>
 
 #if APP_WIN32
 #include <Windows.h>
@@ -15,8 +17,14 @@ int CALLBACK WinMain(_In_ HINSTANCE aInst, _In_opt_ HINSTANCE pInst, _In_ LPSTR 
 
 int main(int argc, char* argv[])
 {
+    ost::CCommandLineParser cmdParser = ost::CCommandLineParser(argv, argc);
 
     std::cout << "Attempting to load " <<  GAME_MODULE_DLL_NAME << std::endl;
+
+    std::cout << "Running with config" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "Width: " << ost::command_line::ReadFromParser(ost::command_line::WinWidth, cmdParser) << std::endl;
+    std::cout << "Height: " << ost::command_line::ReadFromParser(ost::command_line::WinHeight, cmdParser) << std::endl;
 
     ostengine_internal::CGameModuleLoader moduleLoader;
 
