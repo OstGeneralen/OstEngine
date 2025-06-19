7bd52b802a38d3bd6bdbdcc7315f937af58737de
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-19 09:11:25 +0100

    Static assert if using unsupported value type for math constants

--- OstEngine/Engine/Include/OstEngine/Math/CommonMath.h
+++ OstEngine/Engine/Include/OstEngine/Math/CommonMath.h
@@ -11,17 +11,20 @@ namespace ost
 	{
 		namespace constants
 		{
-			template<typename T> constexpr static T PI = 3;
+			template<typename T>
+			constexpr static T UNSUPPORTED_CONSTANT_TYPE() { static_assert(false); }
+
+			template<typename T> constexpr static T PI = UNSUPPORTED_CONSTANT_TYPE<T>();
 			template<> constexpr static float32 PI<float32> = 3.1416f;
 			template<> constexpr static float64 PI<float64> = 3.141592;
 
-			template<typename T> constexpr static T DEG_TO_RAD = 0;
+			template<typename T> constexpr static T DEG_TO_RAD = UNSUPPORTED_CONSTANT_TYPE<T>();
 			template<> constexpr static float32 DEG_TO_RAD<float32> = PI<float32> / 180.0f;
-			template<> constexpr static float64 DEG_TO_RAD<float64> = PI<float64> / 180.0f;
+			template<> constexpr static float64 DEG_TO_RAD<float64> = PI<float64> / 180.0;
 
-			template<typename T> constexpr static T RAD_TO_DEG = 0;
+			template<typename T> constexpr static T RAD_TO_DEG = UNSUPPORTED_CONSTANT_TYPE<T>();
 			template<> constexpr static float32 RAD_TO_DEG<float32> = 180.0f / PI<float32>;
-			template<> constexpr static float64 RAD_TO_DEG<float64> = 180.0f / PI<float64>;
+			template<> constexpr static float64 RAD_TO_DEG<float64> = 180.0 / PI<float64>;
 		}
 
 		extern float32 Sin(Radians r);
