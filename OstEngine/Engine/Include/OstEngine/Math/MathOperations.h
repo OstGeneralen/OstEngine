3503ca600b21552a8e2edba93b464c38ebe76485
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-19 07:36:19 +0100

    Math operations wrapper for sin,cos,tan,sqrt etc

---
+++ OstEngine/Engine/Include/OstEngine/Math/MathOperations.h
@@ -0,0 +1,23 @@
+// OstEngine - Copyright(c) 2025 Kasper Esbjörnsson (MIT License)
+#pragma once
+#include "OstEngine/BasicTypes.h"
+#include "OstEngine/Math/Units/Radians.h"
+
+// ------------------------------------------------------------
+
+namespace ost
+{
+	namespace math
+	{
+		extern float32 Sin(Radians r);
+		extern float32 Cos(Radians r);
+		extern float32 Tan(Radians r);
+
+		extern float32 Sqrt(float32 v);
+		extern float64 Sqrt(float64 v);
+	}
+}
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+// ------------------------------------------------------------
