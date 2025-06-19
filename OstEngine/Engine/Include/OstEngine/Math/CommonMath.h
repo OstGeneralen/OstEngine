8b84de45e8ffa4f8554155c3cb5e6321271314fb
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-19 07:57:39 +0100

    Add flt32 and flt64 constants for PI and rad-deg conversions

--- OstEngine/Engine/Include/OstEngine/Math/CommonMath.h
+++ OstEngine/Engine/Include/OstEngine/Math/CommonMath.h
@@ -9,6 +9,21 @@ namespace ost
 {
 	namespace math
 	{
+		namespace constants
+		{
+			template<typename T> constexpr static T PI = 3;
+			template<> constexpr static float32 PI<float32> = 3.1416f;
+			template<> constexpr static float64 PI<float64> = 3.141592;
+
+			template<typename T> constexpr static T DEG_TO_RAD = 0;
+			template<> constexpr static float32 DEG_TO_RAD<float32> = PI<float32> / 180.0f;
+			template<> constexpr static float64 DEG_TO_RAD<float64> = PI<float64> / 180.0f;
+
+			template<typename T> constexpr static T RAD_TO_DEG = 0;
+			template<> constexpr static float32 RAD_TO_DEG<float32> = 180.0f / PI<float32>;
+			template<> constexpr static float64 RAD_TO_DEG<float64> = 180.0f / PI<float64>;
+		}
+
 		extern float32 Sin(Radians r);
 		extern float32 Cos(Radians r);
 		extern float32 Tan(Radians r);
