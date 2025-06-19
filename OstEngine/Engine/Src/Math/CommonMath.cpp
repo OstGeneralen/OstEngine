d27ae3d1af05c9d6c4dfcce2ad3fbfbc7974bf73
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-19 09:12:16 +0100

    Add missing divider comments in CommonMath.cpp

--- OstEngine/Engine/Src/Math/CommonMath.cpp
+++ OstEngine/Engine/Src/Math/CommonMath.cpp
@@ -9,20 +9,28 @@ ost::float32 ost::math::Sin(Radians r)
 	return sinf(r.Get());
 }
 
+// ------------------------------------------------------------
+
 ost::float32 ost::math::Cos(Radians r)
 {
 	return cosf(r.Get());
 }
+
+// ------------------------------------------------------------
 
 ost::float32 ost::math::Tan(Radians r)
 {
 	return tanf(r.Get());
 }
 
+// ------------------------------------------------------------
+
 ost::float32 ost::math::Sqrt(float32 v)
 {
 	return sqrtf(v);
 }
+
+// ------------------------------------------------------------
 
 ost::float64 ost::math::Sqrt(float64 v)
 {
