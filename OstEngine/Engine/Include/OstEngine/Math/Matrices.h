8642291beebad07d833982cd184eb80d6a63f57c
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-25 14:18:21 +0100

    Add combined header for all matrices like vectors

---
+++ OstEngine/Engine/Include/OstEngine/Math/Matrices.h
@@ -0,0 +1,17 @@
+// OstEngine - Copyright(c) 2025 Kasper Esbjörnsson (MIT License)
+#pragma once
+#include "OstEngine/Math/Matrices/Mat3x3.h"
+#include "OstEngine/Math/Matrices/Mat4x4.h"
+
+// ------------------------------------------------------------
+
+namespace ost
+{
+	using Matrix3x3f = TMatrix3x3<float32>;
+	using Matrix4x4f = TMatrix4x4<float32>;
+
+	using Matrix3x3d = TMatrix3x3<float64>;
+	using Matrix4x4d = TMatrix4x4<float64>;
+}
+
+// ------------------------------------------------------------
