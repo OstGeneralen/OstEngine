e69e3a7085942a2741a57952573e9b0e19cf3a36
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-25 14:18:40 +0100

    Add core math include pulling in vectors, matrices and common math

---
+++ OstEngine/Engine/Include/OstEngine/Math/MathCore.h
@@ -0,0 +1,9 @@
+// OstEngine - Copyright(c) 2025 Kasper Esbjörnsson (MIT License)
+#pragma once
+#include "OstEngine/Math/CommonMath.h"
+#include "OstEngine/Math/Vectors.h"
+#include "OstEngine/Math/Matrices.h"
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+// ------------------------------------------------------------
