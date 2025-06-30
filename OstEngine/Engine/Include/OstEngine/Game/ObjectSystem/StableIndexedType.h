cb871c870d2cafadd795de21a5f87b98e4545360
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-30 16:15:32 +0100

    Fix macro for Stable indexed type ordering the sought for contract after its declaration

--- OstEngine/Engine/Include/OstEngine/Game/ObjectSystem/StableIndexedType.h
+++ OstEngine/Engine/Include/OstEngine/Game/ObjectSystem/StableIndexedType.h
@@ -8,14 +8,15 @@
 // Simply place this macro in the body of your type to make
 // it identifiable via a stable ID
 // ------------------------------------------------------------
-#define STABLE_IDX_TYPE(t) private: friend ost::TStableIndexedTypeContainer<t>; ost::SStableIndex _stableIndex
+#define STABLE_IDX_CLASS_TYPE(t) private: ost::SStableIndex _stableIndex; public: ost::SStableIndex GetStableIndex() const { return _stableIndex; } private: friend ost::TStableIndexedTypeContainer<t>;
+#define STABLE_IDX_STRUCT_TYPE(t) private: ost::SStableIndex _stableIndex; public: ost::SStableIndex GetStableIndex() const { return _stableIndex; } private: friend ost::TStableIndexedTypeContainer<t>; public:
 // ------------------------------------------------------------
 
 namespace ost
 {
 	template<typename T> concept StableIDMemberType = requires(T t)
 	{
-		{ t._stableIndex } -> std::same_as<SStableIndex>;
+		{ t.GetStableIndex() } -> std::same_as<SStableIndex>;
 	};
 
 	// Forward declare of the container to allow friending in types
