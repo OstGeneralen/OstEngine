bde1b1667c27d679f3550a1d53e8b3bb10c697c5
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-12-10 07:33:02 +0000

    Typedefinitions for sized basic types

---
+++ Engine/OstEngine/Types.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// ------------------------------------------------------------
+
+#define SIZED_TYPEDEF( Name, Type, Bytes )                                                                             \
+    using Name = Type;                                                                                                 \
+    static_assert( sizeof( Name ) == Bytes );
+
+// ------------------------------------------------------------
+
+SIZED_TYPEDEF(Int8, signed char, 1)
+SIZED_TYPEDEF(Uint8, unsigned char, 1 )
+
+SIZED_TYPEDEF(Int16, signed short, 2)
+SIZED_TYPEDEF(Uint16, unsigned short, 2)
+
+SIZED_TYPEDEF(Int32, signed int, 4)
+SIZED_TYPEDEF(Uint32, unsigned int, 4)
+
+SIZED_TYPEDEF(Int64, signed long long, 8)
+SIZED_TYPEDEF(Uint64, unsigned long long, 8)
+
+SIZED_TYPEDEF(Float32, float, 4)
+SIZED_TYPEDEF(Float64, double, 8)
+
+using CStyleString = char*;
+
+// ------------------------------------------------------------
