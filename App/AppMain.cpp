f9d00b681ee73c74996c98017b1cd66368db1dba
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-12-10 07:36:44 +0000

    Create app using the application base from engine

--- App/AppMain.cpp
+++ App/AppMain.cpp
@@ -1,32 +1,30 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <OstEngine/Application/AppBase.h>
+
+// ------------------------------------------------------------
+
+class CApp : public ost::COstEngineApp
+{
+    ost::Vector2i WindowDimensions() const override
+    {
+        return { 800, 600 };
+    }
+
+    std::string WindowTitle() const override
+    {
+        return "Application";
+    }
+};
 
 // ------------------------------------------------------------
 
 int main( int aArgCount, char* aArgs[] )
 {
-    SDL_Window* winPtr;
-    SDL_Renderer* rendererPtr;
-    bool run = SDL_CreateWindowAndRenderer( "Title", 800, 800, SDL_WINDOW_INPUT_FOCUS, &winPtr, &rendererPtr );
-    SDL_SetRenderDrawColor( rendererPtr, 50, 150, 255, 255 );
-
-    while ( run )
-    {
-        SDL_Event e;
-        while ( SDL_PollEvent( &e ) )
-        {
-            if ( e.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED )
-            {
-                run = false;
-            }
-        }
-
-        SDL_RenderClear( rendererPtr );
-        SDL_RenderPresent( rendererPtr );
-    }
-
-    SDL_DestroyRenderer( rendererPtr );
-    SDL_DestroyWindow( winPtr );
+    CApp app;
+    app.Init();
+    app.Run();
+    app.DeInit();
     return 0;
 }
 
