1955345f955a1107f5fd58e142dd2acca63cc2d8
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-12-12 11:18:05 +0000

    Inverse and rotation matrix 3x3

---
+++ Engine/OstEngine/Math/Matrix3x3.h
@@ -0,0 +1,173 @@
+#pragma once
+#include <OstEngine/Math/AngleUnit.h>
+#include <OstEngine/Math/Matrix2x2.h>
+
+namespace ost
+{
+    template <typename T>
+    class TMatrix3x3
+    {
+    public:
+        T M11, M21, M31;
+        T M12, M22, M32;
+        T M13, M23, M33;
+
+    public:
+        TMatrix3x3();
+        TMatrix3x3( T a11, T a21, T a31, T a12, T a22, T a32, T a13, T a23, T a33 );
+        TMatrix3x3( const TMatrix3x3& ) = default;
+
+        static TMatrix3x3 RotationX( math::Radians r );
+        static TMatrix3x3 RotationY( math::Radians r );
+        static TMatrix3x3 RotationZ( math::Radians r );
+
+        TMatrix3x3& Transpose();
+        TMatrix3x3 GetTransposed() const;
+
+        TMatrix3x3 GetInverse() const;
+
+        T GetDeterminant() const;
+    };
+
+    template <typename T>
+    static TMatrix3x3<T> operator*( const TMatrix3x3<T>& aFirst, const TMatrix3x3<T>& aSecond );
+
+    template <typename T>
+    inline TMatrix3x3<T> ost::TMatrix3x3<T>::RotationX( math::Radians r )
+    {
+        return TMatrix3x3<T>{ 1, 0, 0, 0, cos( r.Get() ), sin( r.Get() ), 0, -sin( r.Get() ), cos( r.Get() ) };
+    }
+
+    template <typename T>
+    inline TMatrix3x3<T> ost::TMatrix3x3<T>::RotationY( math::Radians r )
+    {
+        return TMatrix3x3<T>{ cos( r.Get() ), 0, -sin( r.Get() ), 0, 1, 0, sin( r.Get() ), 0, cos( r.Get() ) };
+    }
+
+    template <typename T>
+    inline TMatrix3x3<T> ost::TMatrix3x3<T>::RotationZ( math::Radians r )
+    {
+        return TMatrix3x3<T>{ cos( r.Get() ), sin( r.Get() ), 0, -sin( r.Get() ), cos( r.Get() ), 0, 0, 0, 1 };
+    }
+
+    template <typename T>
+    inline ost::TMatrix3x3<T>::TMatrix3x3()
+        : M11{ static_cast<T>( 1 ) }
+        , M22{ static_cast<T>( 1 ) }
+        , M33{ static_cast<T>( 1 ) }
+        , M21{ 0 }
+        , M23{ 0 }
+        , M31{ 0 }
+        , M12{ 0 }
+        , M32{ 0 }
+        , M13{ 0 }
+    {
+    }
+
+    template <typename T>
+    inline ost::TMatrix3x3<T>::TMatrix3x3( T a11, T a21, T a31, T a12, T a22, T a32, T a13, T a23, T a33 )
+        : M11{ a11 }
+        , M22{ a22 }
+        , M33{ a33 }
+        , M21{ a21 }
+        , M23{ a23 }
+        , M31{ a31 }
+        , M12{ a12 }
+        , M32{ a32 }
+        , M13{ a13 }
+    {
+    }
+
+    template <typename T>
+    inline TMatrix3x3<T>& ost::TMatrix3x3<T>::Transpose()
+    {
+        std::swap( M21, M12 );
+        std::swap( M31, M13 );
+        std::swap( M23, M32 );
+        return *this;
+    }
+
+    template <typename T>
+    inline TMatrix3x3<T> ost::TMatrix3x3<T>::GetTransposed() const
+    {
+        TMatrix3x3<T> tm = *this;
+        return tm.Transpose();
+    }
+
+    template <typename T>
+    TMatrix3x3<T> operator*( const TMatrix3x3<T>& aFirst, const TMatrix3x3<T>& aSecond )
+    {
+        TMatrix3x3<T> result;
+        result.M11 = ( aFirst.M11 * aSecond.M11 ) + ( aFirst.M21 * aSecond.M12 ) + ( aFirst.M31 * aSecond.M13 );
+        result.M12 = ( aFirst.M12 * aSecond.M11 ) + ( aFirst.M22 * aSecond.M12 ) + ( aFirst.M32 * aSecond.M13 );
+        result.M13 = ( aFirst.M13 * aSecond.M11 ) + ( aFirst.M23 * aSecond.M12 ) + ( aFirst.M33 * aSecond.M13 );
+
+        result.M21 = ( aFirst.M11 * aSecond.M21 ) + ( aFirst.M21 * aSecond.M22 ) + ( aFirst.M31 * aSecond.M23 );
+        result.M22 = ( aFirst.M12 * aSecond.M21 ) + ( aFirst.M22 * aSecond.M22 ) + ( aFirst.M32 * aSecond.M23 );
+        result.M23 = ( aFirst.M13 * aSecond.M21 ) + ( aFirst.M23 * aSecond.M22 ) + ( aFirst.M33 * aSecond.M23 );
+
+        result.M31 = ( aFirst.M11 * aSecond.M31 ) + ( aFirst.M21 * aSecond.M32 ) + ( aFirst.M31 * aSecond.M33 );
+        result.M32 = ( aFirst.M12 * aSecond.M31 ) + ( aFirst.M22 * aSecond.M32 ) + ( aFirst.M32 * aSecond.M33 );
+        result.M33 = ( aFirst.M13 * aSecond.M31 ) + ( aFirst.M23 * aSecond.M32 ) + ( aFirst.M33 * aSecond.M33 );
+        return result;
+    }
+
+    template <typename T>
+    inline TMatrix3x3<T> ost::TMatrix3x3<T>::GetInverse() const
+    {
+        // Calculate minors
+        const T minor11 = TMatrix2x2<T>( M22, M32, M23, M33 ).GetDeterminant();
+        const T minor21 = TMatrix2x2<T>( M12, M32, M13, M33 ).GetDeterminant();
+        const T minor31 = TMatrix2x2<T>( M12, M22, M13, M23 ).GetDeterminant();
+
+        const T minor12 = TMatrix2x2<T>( M21, M31, M23, M33 ).GetDeterminant();
+        const T minor22 = TMatrix2x2<T>( M11, M31, M13, M33 ).GetDeterminant();
+        const T minor32 = TMatrix2x2<T>( M11, M21, M13, M23 ).GetDeterminant();
+
+        const T minor13 = TMatrix2x2<T>( M21, M31, M22, M32 ).GetDeterminant();
+        const T minor23 = TMatrix2x2<T>( M11, M31, M12, M32 ).GetDeterminant();
+        const T minor33 = TMatrix2x2<T>( M11, M21, M12, M22 ).GetDeterminant();
+
+        // Cofactors
+        const T cofactor11 = minor11;
+        const T cofactor21 = -minor21;
+        const T cofactor31 = minor31;
+        const T cofactor12 = -minor12;
+        const T cofactor22 = minor22;
+        const T cofactor32 = -minor32;
+        const T cofactor13 = minor13;
+        const T cofactor23 = -minor23;
+        const T cofactor33 = minor33;
+
+        // Result is the transpose of cofactors
+        TMatrix3x3<T> result;
+        result.M11 = cofactor11;
+        result.M21 = cofactor12;
+        result.M31 = cofactor13;
+
+        result.M12 = cofactor21;
+        result.M22 = cofactor22;
+        result.M32 = cofactor23;
+
+        result.M13 = cofactor31;
+        result.M23 = cofactor32;
+        result.M33 = cofactor33;
+
+        return result;
+    }
+
+    template <typename T>
+    inline T ost::TMatrix3x3<T>::GetDeterminant() const
+    {
+        const T posFactor1 = M11 * M22 * M33;
+        const T posFactor2 = M21 * M32 * M13;
+        const T posFactor3 = M31 * M12 * M23;
+
+        const T negFactor1 = M11 * M32 * M23;
+        const T negFactor2 = M21 * M12 * M33;
+        const T negFactor3 = M31 * M22 * M13;
+
+        return posFactor1 + posFactor2 + posFactor3 - negFactor1 - negFactor2 - negFactor3;
+    }
+
+} // namespace ost
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <OstEngine/Math/Matrix4x4.h>
 
 CGame gameInstance;
 ost::IGame* GameMain()
@@ -8,6 +9,32 @@ ost::IGame* GameMain()
 
 void CGame::Load()
 {
+    ost::TMatrix4x4<float> ident;
+    ost::TMatrix4x4<float> trans;
+    trans.M14 = 10;
+    trans.M24 = 10;
+    trans.M34 = 10;
+
+    ost::TMatrix4x4<float> loc;
+    loc.M14 = 2;
+    loc.M24 = 4;
+    loc.M34 = 6;
+
+    ost::TMatrix4x4<float> mulIdent = trans * ident;
+    ost::TMatrix4x4<float> mulLoc = trans * loc;
+
+    const float identX = mulIdent.M14;
+    const float identY = mulIdent.M24;
+    const float identZ = mulIdent.M34;
+
+    const float mlocX = mulLoc.M14;
+    const float mlocY = mulLoc.M24;
+    const float mlocZ = mulLoc.M34;
+
+    ost::TVector4<float> testVec{2,4,0,1};
+
+    auto resVec = loc * testVec;
+
     _moveInputAction = ost::SInputAction( INPUT_CALLBACK( InputMove ) );
     {
         using namespace ost;
